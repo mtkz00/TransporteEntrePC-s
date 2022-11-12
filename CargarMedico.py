3d7f3344293830afe2e3118deb0175d431237509
Joselo Esteban Orellano <95329694+JEOrellano@users.noreply.github.com>
2022-11-12 19:23:19 -0300

    Error de syntaxis
    Faltaban comas
    faltaban parámetros al poner ??
    se repetía clave primaría DNI y matricula

--- CargarMedico.py
+++ CargarMedico.py
@@ -9,18 +9,18 @@ miCursor.execute("""
         Nombre varchar (45) not null,
         Apellido  varchar (45) not null,
         Telefono varchar (45) not null,
-        Celular varchar (45) not null
-        Localidad varchar (45) not null
+        Celular varchar (45) not null,
+        Localidad varchar (45) not null,
         Email varchar (45) not null
         )
 """)
 medicos = [
-    (11111111, 10000011 , 'Carlos', 'Gonzalez', '42000000', '42100000', 'Cordoba', 'doctor0@mail.com'),
-    (22222222, 20000012 , 'Roman', 'Solmi', '4300000', '4310000', 'Cordoba', 'doctor1@mail.com'),
-    (11111111, 10000011 , 'Pedro', 'Perez', '450000', '441000', 'Cordoba', 'doctor2@mail.com'),
-    (11111111, 10000011 , 'Lucas', 'Suarez', '46000', '48100000', 'Cordoba', 'doctor3@mail.com'),
+    (11111111, 10000011, 'Carlos', 'Gonzalez', '42000000', '42100000', 'Cordoba', 'doctor0@mail.com'),
+    (22222222, 20000012, 'Roman', 'Solmi', '4300000', '4310000', 'Cordoba', 'doctor1@mail.com'),
+    (33333333, 30000014, 'Pedro', 'Perez', '450000', '441000', 'Cordoba', 'doctor2@mail.com'),
+    (44444444, 40000015, 'Lucas', 'Suarez', '46000', '48100000', 'Cordoba', 'doctor3@mail.com'),
 ]
-miCursor.executemany("INSERT INTO UsuarioMedico VALUES (?, ?, ?, ?, ?, ?)", medicos)
-miCursor.execute("INSERT INTO UsuarioMedico VALUES (11111111, 10000011 , 'Carlos', 'Gonzalez', '42000000', '42100000', 'Cordoba', 'doctor0@mail.com')")
+miCursor.executemany("INSERT INTO UsuarioMedico VALUES (?, ?, ?, ?, ?, ?, ?, ?)", medicos)
+miCursor.execute("INSERT INTO UsuarioMedico VALUES (55555555, 50000015 , 'Carlos', 'Gonzalez', '42000000', '42100000', 'Cordoba', 'doctor0@mail.com')")
 miConexion.commit()
 miConexion.close()
